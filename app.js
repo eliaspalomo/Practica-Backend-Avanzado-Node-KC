5e0f5b22a074ee8ff0622583f936484fb6a782da
Elias Palomo <elias.palomo@gmail.com>
2021-04-24 11:33:45 +0200

    inicial2

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
  
 var app = express();
 
-require('./lbi/connectMongoose');
+require('./lib/connectMongoose');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
